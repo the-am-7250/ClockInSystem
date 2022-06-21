16bd7952581a632914a2e39cd6927b58773685e6
Aarohan Mishra <am9705@rit.edu>
2022-06-21 22:23:29 +0530

    Added an object for Date class in code, still trying to figure out how to use timer atm

--- src/ShiftThread.java
+++ src/ShiftThread.java
@@ -1,19 +1,23 @@
 package src;
+import java.util.Date;
 import java.util.Timer;
 
 public class ShiftThread extends Thread{
     private String empNum;
+    private Timer x;
+    private Date date;
     public ShiftThread(String empNum){
         this.empNum= empNum;
-        Timer x= new Timer(empNum);
+        this.date= new Date();
+        this.x= new Timer(empNum);
     }
     @Override
     public void run(){
         if(this.isAlive()){
-
+            //
         }
         else{
-
+            //x.schedule();
         }
     }
 }
