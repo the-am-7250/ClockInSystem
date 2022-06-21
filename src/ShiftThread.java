d79b5b7719b75f6024417839df52ebd41af6dd6a
Aarohan Mishra <am9705@rit.edu>
2022-06-21 17:44:16 +0530

    Added more code for the ShiftThread class

--- src/ShiftThread.java
+++ src/ShiftThread.java
@@ -1,6 +1,12 @@
 package src;
+import java.util.Timer;
 
 public class ShiftThread extends Thread{
+    private String empNum;
+    public ShiftThread(String empNum){
+        this.empNum= empNum;
+        Timer x= new Timer(empNum);
+    }
     @Override
     public void run(){
         if(this.isAlive()){
