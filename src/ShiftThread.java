8976d562de68ee8bdddd4fcee2dec14c362f11e2
Aarohan Mishra <am9705@rit.edu>
2022-07-10 12:17:55 +0530

    Added more code to ShiftThread.java . No previous code in recent days due to travel without laptop. Hope to be more regular in commits

--- src/ShiftThread.java
+++ src/ShiftThread.java
@@ -1,23 +1,35 @@
+/**
+ * File name: ShiftThread.java
+ * @Author: Aarohan Mishra
+ */
 package src;
 import java.util.Date;
 import java.util.Timer;
+import java.lang.Thread;
+import java.lang.System;
 
 public class ShiftThread extends Thread{
+    /**Variable to store the employees Unique Identification Number (UID)*/
     private String empNum;
     private Timer x;
     private Date date;
+    private long startTime;
+    private long endTime;
     public ShiftThread(String empNum){
         this.empNum= empNum;
         this.date= new Date();
         this.x= new Timer(empNum);
+        this.startTime= 0;
+        this.endTime= 0;
     }
     @Override
     public void run(){
-        if(this.isAlive()){
-            //
+        if(Thread.currentThread().isAlive()){
+            this.endTime= System.currentTimeMillis();
+
         }
         else{
-            //x.schedule();
+            this.startTime= System.currentTimeMillis();
         }
     }
 }
